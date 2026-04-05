8c09d3e79eaa5f2c558482b7860e527229921238
Lalita Chaurasia <chaurasialalita17@gmail.com>
2026-04-05 13:25:38 +0530

    Fix API URL to live server

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,7 +3,7 @@ AOS.init();
 // Contact Form 
 document.getElementById("contactForm").addEventListener("submit",function(e){
 	 e.preventDefault();
-	 fetch("/contact",{ method:"POST", headers:{"Content-Type":"application/json"},
+	 fetch("https://vidya-mandir.onrender.com/contact",{ method:"POST", headers:{"Content-Type":"application/json"},
 	 body:JSON.stringify({ name:document.getElementById("name").value,
 	 email:document.getElementById("email").value,
 	 message:document.getElementById("message").value }) }).then(()=>alert("Message Sent")); });
