e2bc86255952523da90dcdf2e24c6cd4a671b126
Lalita Chaurasia <chaurasialalita17@gmail.com>
2026-04-06 12:53:54 +0530

    Update Js file

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -8,16 +8,3 @@ document.getElementById("contactForm").addEventListener("submit",function(e){
 	 email:document.getElementById("email").value,
 	 message:document.getElementById("message").value }) }).then(()=>alert("Message Sent")); });
 	 
-
-// Login
- function login(){ fetch("/login",{ method:"POST", headers:{"Content-Type":"application/json"},
-  body:JSON.stringify({ username:document.getElementById("user").value,
-	 password:document.getElementById("pass").value }) }).then(r=>r.text()).then(d=>alert(d)); }
-
-// Add Student
- function addStudent(){ fetch("/student",{ method:"POST", headers:{"Content-Type":"application/json"},
- body:JSON.stringify({ name:document.getElementById("sname").value,
-	 email:document.getElementById("semail").value,
-	 course:document.getElementById("course").value }) }).then(()=>alert("Student Added")); }
-
-// Load Students function loadStudents(){ fetch("http://localhost:8080/student/all") .then(r=>r.json()) .then(data=>{ let out=""; data.forEach(s=>{ out+=<p>${s.name} - ${s.course}</p>; }); document.getElementById("students").innerHTML=out; }); }
